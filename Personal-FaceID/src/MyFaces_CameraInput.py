ffc4ef3f9980f037ffb5344004752c7d43c1f13c
YanagiFive <kanaliushijun@gmail.com>
2018-11-26 18:35:15 +0800

    bug fix

--- Personal-FaceID/src/MyFaces_CameraInput.py
+++ Personal-FaceID/src/MyFaces_CameraInput.py
@@ -11,22 +11,25 @@ if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
 # 改变图片的亮度与对比度
+
+
 def relight(img, light=1, bias=0):
     w = img.shape[1]
     h = img.shape[0]
     #image = []
-    for i in range(0,w):
-        for j in range(0,h):
+    for i in range(0, w):
+        for j in range(0, h):
             for c in range(3):
-                tmp = int(img[j,i,c]*light + bias)
+                tmp = int(img[j, i, c]*light + bias)
                 if tmp > 255:
                     tmp = 255
                 elif tmp < 0:
                     tmp = 0
-                img[j,i,c] = tmp
+                img[j, i, c] = tmp
     return img
 
-#使用dlib自带的frontal_face_detector作为我们的特征提取器
+
+# 使用dlib自带的frontal_face_detector作为我们的特征提取器
 detector = dlib.get_frontal_face_detector()
 # 打开摄像头 参数为输入流，可以为摄像头或视频文件
 camera = cv2.VideoCapture(0)
@@ -48,11 +51,12 @@ while True:
             x2 = d.left() if d.left() > 0 else 0
             y2 = d.right() if d.right() > 0 else 0
 
-            face = img[x1:y1,x2:y2]
+            face = img[x1:y1, x2:y2]
             # 调整图片的对比度与亮度， 对比度与亮度值都取随机数，这样能增加样本的多样性
-            face = relight(face, random.uniform(0.5, 1.5), random.randint(-50, 50))
+            face = relight(face, random.uniform(
+                0.5, 1.5), random.randint(-50, 50))
 
-            face = cv2.resize(face, (size,size))
+            face = cv2.resize(face, (size, size))
 
             cv2.imshow('image', face)
 
@@ -63,5 +67,5 @@ while True:
         if key == 27:
             break
     else:
-        print('Finished!')
+        print('Process Done!')
         break
